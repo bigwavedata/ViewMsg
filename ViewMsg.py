f7729d4316049ba81eaa6099471814c18f570162
Oliver <unilateral.abort@gmail.com>
2014-08-01 14:15:40 -0700

    working version

--- ViewMsg.py
+++ ViewMsg.py
@@ -12,26 +12,45 @@ import easygui as GUI
 
 
 
-def checkFile(name):    
-    assert converter.isOleFile("Example Message Subject.msg")
+def checkFile(name):  
+    try:  
+        if not converter.isOleFile(name):
+            sys.exit()       
+    except:
+        print "ERROR: %s is not a *.msg format file" % name
+        sys.exit()
+        
+def getSubject(message):
+    
+    Date=''
+    From=''
+    To=''
+    Subject=''
+    
+    if message.date:
+        Date="Date: " + message.date
+    if message.subject:
+        Subject="Subject: " + message.subject
+    if message.sender:
+        From="From: " + message.sender
+    if message.to:
+        To="To: " + message.to
+    
+    msgString = "%s\n%s\n%s\n%s\n" % (Date,Subject,From,To)        
+    
+    return msgString
 
 def doConversion(name):
     checkFile(name)
     ole = converter.OleFileIO(name)
-    #data = ole.open('__nameid_version1.0')
     print ole.listdir(True, True)
     meta= ole.get_metadata()
     meta.dump()
-
-# copy and import easyGUI in order to have the textbox 
-
-    
     
 if __name__ == '__main__':
     fileDialog = False
     fileLocation = ''
-    #filename = "file1.msg" 
-    #doConversion(filename)
+    
     if len(sys.argv) <= 1 :
         fileDialog = True
     elif len(sys.argv) == 2 :
@@ -42,12 +61,11 @@ if __name__ == '__main__':
         root.withdraw()
         fileLocation = tkFileDialog.askopenfilename()
     
+    checkFile(fileLocation)
+    
     # fileLocation is set by this point
     message = Message(fileLocation)
     
-    print "the subject:"
-    print "Value", message.subject
-    print "Header",message.header
-    print "the message:"
-    print message.body
-    GUI.textbox(message.subject, "", message.body, 0)
+    subject = getSubject(message)
+    
+    GUI.textbox(subject, "", message.body, 0)
